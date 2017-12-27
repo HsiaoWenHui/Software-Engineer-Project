04b37b6c9c971fa842c5c451679ad8f117d5c817
HsiaoWenHui <1205hsiao@gmail.com>
2017-12-28 01:18:23 +0800

    簡易GUI

--- view.py
+++ view.py
@@ -1,42 +1,55 @@
 import tkinter as tk
-
+import controller,model
 
 class view(object):
     window=tk.Tk()
     window.title('Tetris Battle')
     window.geometry('400x500')
-    
+    tetrisController=controller()
+    state=''
+    play=tk.Button(window,text='play',commond=tetrisController.input('PLAY'))
+    setting=tk.Button(window,text='setting',commond=tetrisController.input('SET'))
+    pause=tk.Button(window,text='pause',commond=tetrisController.input('PAUSE'))
+    esc=tk.Button(window,text='end',commond=tetrisController.input('ESC'))
     def __init__(self):
-        self.state = 'IDLE'
-        
+        self.newState = 'IDLE'
+
     def stateHasChanged(self,model,newState):
         self.gameModel=model
         self.changeView(newState)
         
     def changView(self,newState):
         if(newState==self.gameModel.start_STATE):
-            idle=tk.Label(self.window,text='menu')
-            idle.pack()
+            self.start
         elif(newState==self.gameModel.setting_STATE):
-            idle=tk.Label(self.window,text='setting')
-            idle.pack()
+            self.setting
         elif(newState==self.gameModel.play_STATE):
-            idle=tk.Label(self.window,text='play')
-            idle.pack()
+            self.play
         elif(newState==self.gameModel.pause_STATE):
-            idle=tk.Label(self.window,text='pause')
-            idle.pack()
+            self.pause
     
     def getUserInput(self):
+        self.tetrisController.input(self.state)
+       
+    
+    def start(self):
+        l=tk.Label(self.window,text='Tetris Soft.Engi')
+        self.l.pack()
+        self.play.pack()
+        self.setting.pack()
+        self.esc.pack() 
+    def setting(self):
         
-        play=tk.Button(self.window,text='play')
-        play.pack()
-        setting=tk.Button(self.window,text='setting')
-        setting.pack()
-        esc=tk.Button(self.window,text='end')
-        esc.pack()
-        self.window.mainloop()
+        self.setting.pack()
+    def pause(self):
+       
+        self.esc.pack()
+    def play(self):    
+        self.play.pack()
         
+        
+    window.mainloop()    
 a=view()
 a.getUserInput()
+
     
