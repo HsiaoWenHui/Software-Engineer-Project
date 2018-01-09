2c2a966e865c21404e94803fd6933760784af92c
HsiaoWenHui <1205hsiao@gmail.com>
2018-01-10 04:18:55 +0800

    新增遊戲畫面

--- view.py
+++ view.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
 import tkinter
+import ttk
 from distutils.dist import command_re
 from tkinter.constants import ANCHOR
 from PIL.ImageTk import PhotoImage
 from django.utils.termcolors import background
 import controller
 from doctest import master
-
-
+import numpy as np
     
 class Application(tkinter.Frame):
     def __init__(self, master):
         tkinter.Frame.__init__(self, master)
-        self.master.minsize(width=500, height=500)
-        self.master.config()
-        self.state=""
+        self.master.minsize(width=352, height=660)
         
+        self.state=""
+        self.bg=PhotoImage(file='./back.jpeg')
         self.playImage=PhotoImage(file='./play.png')
         self.setImage=PhotoImage(file='./settings.png')
         self.setImage2=PhotoImage(file='./settings2.png')
@@ -24,21 +24,21 @@ class Application(tkinter.Frame):
         self.pauseImage=PhotoImage(file='./pause.png')
         self.cancelImage=PhotoImage(file='./cancel.png')
         self.restartImage=PhotoImage(file='./restart.png')
-        
+        self.master.config()
         
         self.inputText = tkinter.Label(master,image=self.logoImage,text="tetris")
         self.score=tkinter.Label(master,font=('Arial,80'),width=15,height=2,text="score")
         self.playButton=tkinter.Button(master,image=self.playImage,text='PLAY',width=150,height=80,command=self.play)
+    
         self.settingButton=tkinter.Button(master,image=self.setImage,text='SET',width=150,height=80,command=self.set)
-        self.pauseButton=tkinter.Button(master,image=self.pauseImage,text='PAUSE',width=40,height=40,command=self.pause)
+        self.pauseButton=tkinter.Button(master,image=self.pauseImage,text='PAUSE',width=30,height=30,command=self.pause)
         self.restartButton=tkinter.Button(master,image=self.restartImage,text='RESTART',width=100,height=60,command=self.restart)
-        
         self.cancelButton=tkinter.Button(master,image=self.cancelImage,text='CANCEL',width=60,height=60,command=self.cancel)
-        
-        self.vol=tkinter.StringVar()
-        self.volLarge=tkinter.Radiobutton(text='Large',variable=self.vol,value='large',command=self.voltune)
-        self.volMid=tkinter.Radiobutton(text='Mid',variable=self.vol,value='mid',command=self.voltune)
-        self.volLow=tkinter.Radiobutton(text='Low',variable=self.vol,value='low',command=self.voltune)
+        self.canvas=tkinter.Canvas(borderwidth=2,width=330,height=610)
+        self.panel=tkinter.StringVar()
+        self.panel1=tkinter.Radiobutton(text='1',variable=self.panel,value='1',command=self.panelchange)
+        self.panel2=tkinter.Radiobutton(text='2',variable=self.panel,value='2',command=self.panelchange)
+        self.panel3=tkinter.Radiobutton(text='3',variable=self.panel,value='3',command=self.panelchange)
         self.master.bind('<Left>', self.left_key)
         self.master.bind('<Right>', self.right_key)
         self.master.bind('<Up>', self.up_key)
@@ -47,60 +47,93 @@ class Application(tkinter.Frame):
 
     def changeView(self,state):
             self.state=state
-            if(state=="IDLE"):{
+            if state=="IDLE":
+                self.clear()
                 self.startFrame()
-                }
-            elif(state=="PLAY"):{
+            
+            elif(state=="PLAY"):
+                self.clear()
                 self.playFrame()
-                }
-            elif(state=='SET'):{
+                
+            elif(state=='SET'):
+                self.clear()
                 self.setFrame()
-                }
-            elif(state=='PAUSE'):{
+                
+            elif(state=='PAUSE'):
+                self.clear()
                 self.pauseFrame()
-                }
-            elif(state=='OVER'):{
+                
+            elif(state=='OVER'):
                 self.overFrame()
-                }
-            else:{
+                
+            else:
                 self.startFrame()
-                }
+                
     def startFrame(self):
         
-        startFrame=tkinter.Frame(self)
-        startFrame.place()
-        #self.playButton.config(startFrame)
         
-        self.playButton.place(x=250,y=200,anchor='center')
-       # self.settingButton.config(startFrame)
-        self.settingButton.place(x=250,y=300,anchor='center')
-        self.inputText.place(x=250,y=50,anchor='n')
+        self.playButton.place(x=176,y=300,anchor='center')
+        self.settingButton.place(x=176,y=400,anchor='center')
+        self.inputText.place(x=176,y=100,anchor='n')
+    
     def playFrame(self):
-        self.pack_forget()
-        #playFrame=tkinter.Frame(self)
-       #playFrame.place()
-       # self.score.config(playFrame)
+        
+        
+        self.canvas.pack(side='bottom')
         self.score.place(x=0,y=0,anchor='nw')
-       # self.pauseButton.config(playFrame)
-        self.pauseButton.place(x=250,y=0,anchor='n')
+        self.pauseButton.place(x=176,y=5,anchor='n')
+        outside=self.canvas.create_rectangle(10,5,330,605)#每格40
+        
+        board= np.zeros([15,8])
+      
+        board[1][3]=1
+        board[0][3]=1
+        board[0][0]=1
+        board[0][1]=1
+        board[0][2]=1
+        board[0][4]=1
+        board[0][5]=1
+        board[0][6]=1
+        board[0][7]=1
+        for i in range(0,15):
+            board[i][0]=1
+        for i in range(0,15):
+            for j in range(0,8):
+                if board[i][j]==1:
+                    square=self.canvas.create_rectangle(j*40+10,i*40+5,j*40+50,i*40+45,fill="red")
+                else:
+                    continue
     
     def pauseFrame(self):
-        pauseFrame=tkinter.Frame(self)
-        pauseFrame.place()
         
-        self.restartButton.place(x=250,y=200,anchor='center')
-        self.cancelButton.place(x=180,y=280,anchor='w')
+        self.restartButton.place(x=176,y=200,anchor='center')
+        self.cancelButton.place(x=106,y=280,anchor='w')
         self.settingButton.config(image=self.setImage2,width=60,height=60)
-        self.settingButton.place(x=250,y=280,anchor='w')
+        self.settingButton.place(x=176,y=280,anchor='w')
     
         
     def setFrame(self):
-        self.volLarge.place(x=200,y=200,anchor='center')
-        self.volMid.place(x=250,y=200,anchor='center')
-        self.volLow.place(x=300,y=200,anchor='center')
+        
+        self.panel1.place(x=126,y=200,anchor='center')
+        self.panel2.place(x=176,y=200,anchor='center')
+        self.panel3.place(x=226,y=200,anchor='center')
+        self.cancelButton.place(x=176,y=250,anchor='center')
     def overFrame(self):
+        
         self.inputText.config(text="over")   
         
+    def clear(self):
+        self.canvas.pack_forget()
+        self.inputText.place_forget()
+        self.playButton.place_forget()
+        self.score.place_forget()
+        self.settingButton.place_forget()
+        self.pauseButton.place_forget()
+        self.restartButton.place_forget()
+        self.cancelButton.place_forget()
+        self.panel1.place_forget()
+        self.panel2.place_forget()
+        self.panel3.place_forget()
         
     def play(self):
         control.play() 
@@ -110,8 +143,8 @@ class Application(tkinter.Frame):
         control.pause()
         
                
-    def voltune(self):
-        print (self.vol.get()+" button pressed")
+    def panelchange(self):
+        print (self.view.get()+" button pressed")
         
     def cancel(self):
         if(self.state=="SET"):
