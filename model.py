407216d1ef78589e590a0959162d3a64513ab62a
HuSalt <danny19960112@gmail.com>
2018-01-11 02:34:01 +0800

    move, frozen, fall revise

--- model.py
+++ model.py
@@ -56,8 +56,8 @@ class GameModel(Model):
         
         #all board state
         size = 120
-        for i in range(0,size):    
-            self.board_state.append(0)#nothing
+        for i in range(0,size):
+            self.board_state.append(0)
             self.frozen_board.append(0)
             
         self.New_Block()
@@ -67,7 +67,7 @@ class GameModel(Model):
         outCheck = True
         if self.falling_class == 10 :
             if(temp_Falling[0] % 8 == 6):
-                   outCheck = False              
+                outCheck = False              
             temp_Falling[1] = temp_Falling[1] - 7
             temp_Falling[2] = temp_Falling[2] - 2
             temp_Falling[3] = temp_Falling[3] - 9
@@ -78,8 +78,8 @@ class GameModel(Model):
                     if(j == i):
                         outCheck = False
             for i in temp_Falling:
-                    if((i / 8) > board_long):
-                        outCheck = False
+                if((i / 8) > board_long):
+                    outCheck = False
                         
             #若沒有撞到frozen或是邊緣則可進行旋轉
             if(outCheck):
@@ -170,6 +170,7 @@ class GameModel(Model):
                 for i in range(4):
                     self.falling_class += 1
                     self.falling_block[i] = temp_Falling[i] 
+            
             
         elif self.falling_class == 31:
             if(temp_Falling[0] % 8 == 0):
@@ -375,6 +376,7 @@ class GameModel(Model):
                     self.falling_class += 1
                     self.falling_block[i] = temp_Falling[i]
             
+            
         elif self.falling_class == 52:
             temp_Falling[0] = temp_Falling[0] - 1
             temp_Falling[1] = temp_Falling[1] - 1
@@ -472,93 +474,101 @@ class GameModel(Model):
                     self.falling_block[i] = temp_Falling[i] 
             
     def Move(self, lorr):
+        print(self.board_state)
         tmp = []
+        
         if lorr == 1:#right
+        #check hit
             for x in self.falling_block:
                 var = x + 1
                 place = var % 8
                 if place == 0:
-                    return True#hit boarder 
+                    return True#hit boarder, do nothing 
+                else:
+                    tmp.append(var)                
+                    
+        elif lorr == -1:#left
+            #check hit
+            for x in self.falling_block:
+                var = x - 1
+                place = var % 8
+                if place == 7:
+                    return True#hit boarder, do nothing
                 else:
                     tmp.append(var)
-            if self.Check_Frozen(tmp):
-                self.board_state = self.frozen_board#not hit
-            else:
-                self.falling_block = tmp
-                state = int(self.falling_class / 10)
-                for blc in self.falling_block:
-                    self.frozen_board[blc] = state
+                    
+                    
+        if self.Check_Frozen(tmp):#T hit, F not hit
+            self.board_state = self.frozen_board[:]#not hit
                 
-                    
-        else:#left
-            for x in self.falling_block:
-                var = x + 1
-                place = var % 8
-                if place == 0:
-                    return True
-                else:
-                    tmp.append(var)
-                    
+        else:#not hit, add frozen and falling into board
+            self.board_state = self.frozen_board[:]
+                
+            self.falling_block = tmp
+            state = int(self.falling_class / 10)
+            for blc in self.falling_block:
+                self.board_state[blc] = state
+        print(self.board_state)
             
     def Check_Frozen(self, temp):
-        board_size = 120
-        count = 0
+        flag = False
         for i in temp:
-            if i < board_size:
+            try:
+                #hit block
                 if self.frozen_board[i] != 0:
-                    #hit block input into frozen
-                    count += 1
-            else:#hit boarder
-                count += 1
-                
-        if count == 0:
-            return False
-        else:
+                    flag = True
+                    break
+            #hit boarder
+            except:
+                flag = True
+                break
+        if flag:
             state = int(self.falling_class / 10)
             for blc in self.falling_block:
                 self.frozen_board[blc] = state
             return True
+        
+        else:
+            return False
     
                
     def Check_Erase(self):
-        print(1)
-#        chk_rowStart = []
-#        for block in self.falling_block:
-#            x = block % 8
-#            start = block - x
-#            if not (start in chk_rowStart):
-#                chk_rowStart.append(start)#store all row start need to check
-#                
-#        erase_row = []
-#        for x in chk_rowStart:
-#            i = 0
-#            tmp = x
-#            while not (self.frozen_board[tmp] == 0):
-#                i += 1
-#                tmp = x + i
-#            if i >= self.board[0]:
-#                erase_row.append(x)
-#
-#        #some row need to erase, move down upper block
-#        if erase_row:
-#            temp_board = self.frozen_board
-#            for rowX in erase_row:
-#                i = 0
-#                while i < rowX:
-#                    temp_board[i + 8] = temp_board[i]
-#
-#
-#        if len(erase_row) == 1:
-#            self.point += 10
-#        elif len(erase_row) == 2:
-#            self.point += 30
-#        elif len(erase_row) == 3:
-#            self.point += 70
-#        elif len(erase_row) == 4:
-#            self.point += 150
-#        else:
-#            return
+        chk_rowStart = []
+        for block in self.falling_block:
+            x = block % 8
+            start = block - x
+            if not (start in chk_rowStart):
+                chk_rowStart.append(start)#store all row start need to check
+                
+        erase_row = []
+        for x in chk_rowStart:
+            i = 0
+            tmp = x
+            while not (self.frozen_board[tmp] == 0):
+                i += 1
+                tmp = x + i
+            if i >= self.board[0]:
+                erase_row.append(x)
 
+        #some row need to erase, move down upper block
+        if erase_row:
+            temp_board = self.frozen_board[:]
+            for rowX in erase_row:
+                i = 0
+                while i < rowX:
+                    temp_board[i + 8] = temp_board[i]
+
+
+        if len(erase_row) == 1:
+            self.point += 10
+        elif len(erase_row) == 2:
+            self.point += 30
+        elif len(erase_row) == 3:
+            self.point += 70
+        elif len(erase_row) == 4:
+            self.point += 150
+        else:
+            return
 
     def New_Block(self):
         self.falling_class = random.choice([10, 11, 20, 21, 30, 31, 32, 33, 40, 41, 42, 43, 50, 51, 52, 53, 60, 70, 71])
@@ -576,27 +586,27 @@ class GameModel(Model):
             
     
     def Fall_Down(self):
+        print(self.board_state)
         temp = []#next place
         temp.append(self.falling_block[0] + 8)
         temp.append(self.falling_block[1] + 8)
         temp.append(self.falling_block[2] + 8)
         temp.append(self.falling_block[3] + 8)
         
-        
         #hit
         if self.Check_Frozen(temp):#T hit , F not hit
-            self.Check_Erase()
-            self.board_state = self.frozen_board
+            #self.Check_Erase()
+            self.board_state = self.frozen_board[:]
             return self.New_Block()
         
         #not hit
         else:
             #delete privious falling block, add frozen
-            self.board_state = self.frozen_board
-                        
+            self.board_state = self.frozen_board[:]
+            
             #move down    
             self.falling_block = temp
             state = int(self.falling_class / 10)
             for i in self.falling_block:
                 self.board_state[i] = state#print something exist
-        return True
+            return True
