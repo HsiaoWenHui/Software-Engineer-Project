bd6029dcfa5c61cad2b278da94144fa65e93179b
邵嘉嶸 <s9755059@gmail.com>
2018-01-12 08:34:12 +0800

    add View4
    wait for testing

--- view.py
+++ view.py
@@ -555,3 +555,122 @@ class View_HU(ParentView):
         self.viewflag=int(self.panel.get())
         print (self.panel.get()+" button pressed")
         print(self.viewflag)
+
+        
+class View4(ParentView):
+    def __init__(self, master,model):
+        super().__init__(master, model)
+        
+        self.playImage=PhotoImage(file='./play4.png')
+        self.setImage=PhotoImage(file='./set4.png')
+        self.setImage2=PhotoImage(file='./set4.png')
+        self.logoImage=PhotoImage(file='./logo4.png')
+        self.pauseImage=PhotoImage(file='./pause4.png')
+        self.cancelImage=PhotoImage(file='./cancel.png')
+        self.restartImage=PhotoImage(file='./restart.png')
+        self.master.config(bg="blue")
+        
+        self.inputText = tkinter.Label(master,image=self.logoImage,text="tetris",bg="gray")
+        self.score=tkinter.Label(master,font=("Arial", 15,"bold"),width=11,height=2,text="score : "+self.scoreText,bg="black",fg="white")
+        self.playButton=tkinter.Button(master,image=self.playImage,text='PLAY',width=50,height=50,command=self.play,bg="gray")
+    
+        self.settingButton=tkinter.Button(master,image=self.setImage,text='SET',width=50,height=50,command=self.set,bg="blue")
+        self.pauseButton=tkinter.Button(master,image=self.pauseImage,text='PAUSE',width=30,height=30,command=self.pause,bg="blue")
+        
+        self.cancelButton=tkinter.Button(master,image=self.cancelImage,text='CANCEL',width=80,height=80,command=self.cancel,bg="#0096E6")
+        self.restartButton=tkinter.Button(master,image=self.restartImage,text='RESTART',width=80,height=80,command=self.restart,bg="#0096E6")
+        
+        self.canvas=tkinter.Canvas(width=320,height=600,bg="#E53A40")
+                    
+        
+        self.panel=tkinter.StringVar()
+        self.panel1=tkinter.Radiobutton(text='1',font=('Arial',50),variable=self.panel,value='1',command=self.panelchange,bg="blue",fg="white")
+        self.panel2=tkinter.Radiobutton(text='2',font=('Arial',50),variable=self.panel,value='2',command=self.panelchange,bg="blue",fg="white")
+        self.panel3=tkinter.Radiobutton(text='3',font=('Arial',50),variable=self.panel,value='3',command=self.panelchange,bg="blue",fg="white")
+        self.panel4=tkinter.Radiobutton(text='4',font=('Arial',13),variable=self.panel,value='4',command=self.panelchange,bg="blue",fg="white")
+        self.changPanel=tkinter.Label(master,font=('Arial',20),width=15,height=2,text="Choose Panel",bg="blue",fg="white")
+        self.viewflag=3
+        self.startFrame()
+                
+    def startFrame(self):
+        self.playButton.place(x=176,y=300,anchor='center')
+        self.settingButton.place(x=176,y=400,anchor='center')
+        self.inputText.place(x=176,y=100,anchor='n')
+        
+    def updateGame(self):
+        #print("update")
+        
+        canvas=tkinter.Canvas(self.canvas,width=320,height=600,bg="#FFFFF3")
+        canvas.place(x=0,y=0,width=320,height=600,anchor='nw')
+        outside=canvas.create_rectangle(0,0,320,600)
+        self.score.config(text=str(self.mod.point) + " Ps!")
+        self.score.place(x=0,y=0,anchor='nw')
+      
+        col=0
+        row=0
+       # print(self.mod.board_state)
+        for i in range(0,120):
+            row=int(i/8)
+            col=int(i%8)
+            self.board[row][col]=self.mod.board_state[i]
+            
+        
+        for i in range(0,15):
+            for j in range(0,8):
+                square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#FFFAFA")
+                if self.board[i][j]==1:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#4169E1")#red#B0C4DE
+                elif self.board[i][j]==2:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#DAA520")#blue#30A9DE
+                elif self.board[i][j]==3:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#B0C4DE")#green#8CD790
+                elif self.board[i][j]==4:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#48D1CC")#yellow#EFDC05
+                elif self.board[i][j]==5:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#6B8E23")#purple#A593E0
+                elif self.board[i][j]==6:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#98FB98")#gray#D4DFE6
+                elif self.board[i][j]==7:
+                    square=canvas.create_rectangle(j*40,i*40,j*40+40,i*40+40,fill="#DC143C")#orange#F17F42
+        
+    def playFrame(self):
+        self.canvas.place(x=18,y=45,width=320,height=600,anchor='nw')
+        self.pauseButton.place(x=176,y=5,anchor='n')
+        outside=self.canvas.create_rectangle(0,0,320,600)#每格40
+        
+        
+    def pauseFrame(self):
+        self.restartButton.place(x=100,y=250,anchor='center')
+        self.cancelButton.place(x=250,y=250,anchor='center')
+        
+        
+    def setFrame(self):
+        self.changPanel.place(x=176,y=150,anchor='center')
+        self.panel1.place(x=100,y=200,anchor='center')
+        self.panel2.place(x=150,y=200,anchor='center')
+        self.panel3.place(x=200,y=200,anchor='center')
+        self.panel4.place(x=250,y=200,anchor='center')
+        self.cancelButton.place(x=176,y=300,anchor='center')
+    
+    def clear(self):
+        self.settingButton.config(image=self.setImage,width=50,height=50)
+        self.cancelButton.config(image=self.cancelImage,width=80,height=80,bg="#E53A40")
+        self.changPanel.place_forget()
+        self.canvas.place_forget()
+        self.inputText.place_forget()
+        self.playButton.place_forget()
+        self.score.place_forget()
+        self.settingButton.place_forget()
+        self.pauseButton.place_forget()
+        self.restartButton.place_forget()
+        self.cancelButton.place_forget()
+        self.panel1.place_forget()
+        self.panel2.place_forget()
+        self.panel3.place_forget()
+        self.panel4.place_forget()
+    
+        
+    def panelchange(self):
+        self.viewflag=int(self.panel.get())
+        print (self.panel.get()+" button pressed")
+        print(self.viewflag)
